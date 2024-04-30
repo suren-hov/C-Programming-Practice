a21ae9de977b4a583c62b0f0938ee281c8821a1f
suren-hov <shovhannisyan2021@gmail.com>
2024-04-30 21:11:16 +0400

    Moved printf function to macro

--- vector/print_array.h
+++ vector/print_array.h
@@ -1,13 +1,15 @@
 #ifndef PRINT_ARRAY
 #define PRINT_ARRAY
-
 #include <stdio.h>
+
+#define PRINT_ARRAY_FUNCTION(x, len) \
+    for(int x##_i = 0; x##_i < len; x##_i++){ \
+        printf("%d:%d\n", x##_i, x[x##_i]); \
+    }
 
 int print_array(int *array, int length)
 {
-    for (int i = 0; i < length; i++)
-    {
-        printf("%d:%d\n", i, array[i]);
-    }
+    PRINT_ARRAY_FUNCTION(array, length)
+    return 0;
 }
 #endif // !PRINT_ARRAY
