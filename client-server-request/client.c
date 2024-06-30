8cfe94fdf347c2d12f1234d38d6a6ce43c99215c
suren-hov <shovhannisyan2021@gmail.com>
2024-06-30 20:47:08 +0400

    Added new client server request for training

---
+++ client-server-request/client.c
@@ -0,0 +1,52 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netdb.h>
+#include <arpa/inet.h> 
+#include <unistd.h>
+
+#define PORT_NUMBER 8081
+#define BUFFER_SIZE 1024
+#define IP_ADDRESS "127.0.0.1"
+#define SA struct sockaddr
+
+struct fileParams {
+  int fileSize;
+  int NumberOfCharacters;
+  int NumberOfLines;
+  int NumberOfWords;
+};
+
+void throwError(const char* msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main()
+{
+    int sockfd, connfd;
+	struct sockaddr_in servaddr, cli;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+		throwError("Socket creation failed...\n");
+	} else
+		printf("Socket successfully created..\n");  
+
+    memset((void*)&servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
+    servaddr.sin_port = PORT_NUMBER;
+
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
+		throwError("Connection with the server failed...\n");
+	} else
+		printf("Connected to the server..\n");    
+
+    close(sockfd);
+}
---
+++ client-server-request/server.c
@@ -0,0 +1,84 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#define PORT_NUMBER 8081
+#define BUFFER_SIZE 1024
+#define SA struct sockaddr 
+
+struct fileParams {
+  int fileSize;
+  int NumberOfCharacters;
+  int NumberOfLines;
+  int NumberOfWords;
+};
+
+void throwError(const char* msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+void reciveFileName(int connfd)
+{
+  char buff[BUFFER_SIZE];
+  int n;
+
+  bzero(buff, BUFFER_SIZE); 
+
+  // read the filename from client and save it in buffer 
+  read(connfd, buff, sizeof(buff)); 
+
+  // print buffer which contains the client contents 
+  printf("Filne name is: %s\n", buff);
+
+  bzero(buff, BUFFER_SIZE); 
+  n = 0; 
+
+  write(connfd, buff, sizeof(buff)); 
+}
+
+int main()
+{
+    int sockfd, connfd;
+    socklen_t len; 
+
+   sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1){
+		  throwError("Socket creation failed...\n"); 
+    } else {
+  		printf("Socket successfully created..\n"); 
+    } 
+    
+    struct sockaddr_in server_addr, cli;
+    bzero(&server_addr, sizeof(server_addr)); 
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = PORT_NUMBER;
+
+    if (bind(sockfd, (SA*) &server_addr, sizeof(server_addr)) < 0)
+        throwError("Socket bind failed...\n");
+    else
+		printf("Socket successfully binded..\n"); 
+
+    if (listen(sockfd, 3) != 0)
+        throwError("Listen failed...\n");
+    else
+		printf("Server listening..\n"); 
+
+  	len = sizeof(cli); 
+
+    connfd = accept(sockfd, (SA*) &cli, &len);
+    if (connfd < 0) { 
+      throwError("Server accept failed...\n"); 
+    } else
+      printf("Server accept the client...\n"); 
+
+    reciveFileName(connfd);
+  	close(sockfd); 
+}
